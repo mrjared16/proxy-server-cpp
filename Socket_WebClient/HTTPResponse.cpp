28c32d8ac8bb3e869fa68d0b0880ff7ccabf3ff2
Doan Phuc <mrjared16>
2019-06-22 22:14:26 +0700

    shorten isCache method

--- Socket_WebClient/HTTPResponse.cpp
+++ Socket_WebClient/HTTPResponse.cpp
@@ -11,11 +11,16 @@ HTTPResponse::~HTTPResponse()
 {
 }
 
+//status line = HTTP/1.1 404 Not Found
+//status line = protocol version + status code + status text
+
+
 //HTTP/1.1 404 Not Found (first line)
 //Tu first line (dong dau tien trong du lieu nhan) phan tich va lay ra
 //protocol version, status code va status text
-void HTTPResponse::handle(string status_line)
+void HTTPResponse::handle()
 {
+	string status_line = this->first_line;
 	int start_protocol_version = 0;
 	int start_status_code = status_line.find(" ") + 1;
 	int start_status_text = status_line.find(" ", start_status_code) + 1;
@@ -28,23 +33,18 @@ void HTTPResponse::handle(string status_line)
 //Chi luu lai response neu headers co code = 200 va khong co no-cache
 bool HTTPResponse::isCache()
 {
+	/*
+	return (this->status_code == "200" && this->headers.find("no-cache") == string::npos);
+	*/
 	if (this->status_code != "200") {
 		return false;
 	}
 	else {
-		if (this->header.find("no-cache") != string::npos) {
+		if (this->headers.find("no-cache") != string::npos) {
 			return false;
 		}
 		else {
 			return true;
 		}
 	}
-}
-
-//status line = HTTP/1.1 404 Not Found
-//status line = protocol version + status code + status text
-string HTTPResponse::getStatusLine()
-{
-	string status_line = this->protocol_version + " " + this->status_code + " " + this->status_text;
-	return status_line;
 }
